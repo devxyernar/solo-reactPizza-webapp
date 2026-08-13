b402846fa77c67cda2b3f7fa7eb41d38fe93ad5a
devxyernar <zhaksylyk_ernar@bk.ru>
2026-08-13 19:27:32 +0500

    basic RP 002: foundation for tests

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    enviroment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-});
+})
